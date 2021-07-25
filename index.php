16480e06f80afcc479d4882d5385beef2fb18753
Coder6384 <mwangidavid1067@gmail.com>
2021-07-24 23:10:25 -0700

    minimised index codes

--- index.php
+++ index.php
@@ -1,93 +1,21 @@
+<?php
+include_once './layouts/main/header.php';
 
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!doctype html>
-<html lang="en">
-  <head>
-   
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+?>
 
+<!-- Jumbotron -->
 
-    <link href="./bootstrap-5.0.2-dist/css/bootstrap.min.css"rel="stylesheet"/>
-<link rel="stylesheet" href="main.css/webapp.css">
+<main class="container-fluid">
 
-    
-    <title>Compatibility</title>
-    
-  </head>
-  <body>
-      <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              
-            </a>
-            <button 
-            class="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse"
-             data-bs-target="#navbarSupportedContent" 
-             aria-controls="navbarSupportedContent" 
-             aria-expanded="false"
-              aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="register.php">Sign up</a>
-                  <a class="nav-link" href="login.php">Log In</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Reparing</a></li>
-                    <li><a class="dropdown-item" href="#">Upgrading</a></li>
-                    <li><hr class="dropdown-divider"></li>
-           
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-              </ul>
-              <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
-        </nav>
-      </div>
-    <div class="content-wrapper">
-      <div class="text warpper">
-        <h1>Hadware Compatibility Checker</h1>
-  
-      </div>  
-   <div class="container">
-    <div class="row"></div>
-    <div class="col-12 col-sm-5">
-      <h1>DESCRIPTION</h1>
-    </div>
-    <div class>
-      <p>The system is all about enabling computer owners know what 
-        components their computers have and a vivid description of 
-        each hardware component together with their specific prices</p>
-    </div>
-   
-</div>
+    <h1 class=text-light>Hadware Compatibility Checker</h1>
+    <p class=text-light>
+        The system is all about enabling computer owners know what
+        components their computers have and a vivid description of
+        each hardware component and how they work together together to accomplish
+        the work that we as end users normally expect them to
+    </p>
 
+</main>
 
-  <script src="bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js">
-</script>
-    </div>
-    
-  
-   <body>
-</html>
+<?php
+    include_once './layouts/main/footer.php';
