029ec821d75f8b2a161a5c3a5d1d444b18f2f27c
Coder6384 <mwangidavid1067@gmail.com>
2021-07-24 23:09:02 -0700

    computer literature

---
+++ learn.php
@@ -0,0 +1,72 @@
+<?php
+include_once './layouts/main/header.php';
+
+?>
+
+
+
+<main class="container-fluid">
+
+    <h1 class=text-primary>Learn more on computers</h1>
+    <p class=text-light>
+        It is oftenly difficult for people owning computers to actually know what parts are compatible with their
+        computers whether they are intending to repair, upgrade or assemble their own computers.Bringing the need to
+        explain
+        common computer hardware parts that most likely you will find yourself in need of purchase</p>
+    <h1 class=text-light>Storage</h1>
+    <p class=text-light>Computers have similar ways of storing their data but the difference comes to how the storage
+        location
+        looks like when seen there are the common types known as the Hard Disk Drive or HDD and there is also the solid
+        state drive.
+    </p>
+    <h3 class=text-light>(a) HDDs </h3>
+    <p class=text-light>HDDs come in various types the modern most common one is the SATA found in many computers that
+        have a
+        hard disk drive.It is made up of two spinning disks that have been magnetized, when data is being read from them
+        a sound can be heard, this comes from the head of the hard disk that moves along its arm's reach as it reads and
+        inputs data
+        .It comes at a cheaper price and it is relatively slower than SDDs compapred to the SDD that is why it is mostly
+        preffered by the people who want
+        more storage but compromise on performance</p>
+    <h3 class=text-light>(b) sDDs </h3>
+    <p class=text-light>SDDs lack spining disks like the HDDs but have memory chips inside them, they have been used
+        in
+        phones for the longest
+        time, but are now becoming common in desktops and laptops.They are more durable since they have less moving
+        parts.The lack of moving parts
+        is what makes the SDD costly and also fast</P>
+    <h1 class=text-light>RAM</h1>
+    <p class=text-light>The random Access Memory has various versions depending on how old the computer is or what
+        version your motherboard
+        supports, these versions include DDR, DDR2, DDR3 and DDR4, the later the generation the faster the ram speed in
+        general but
+        each versions also have their own individual clocking speed ranging of upto 4.0ghz found mostly in the DDR4 RAM
+        version.
+        Each compueter motherboard has a particular number of slots its can accommodate ranging from two to four
+        slots.That being
+        said, each motherboard has a limit to how much RAM it can have not doing so might relsult in slowing down of
+        your computer
+    </p>
+    <h1 class=text-light>CPU</h1>
+    <p class=text-light>Even though computer hardware has changed visually over the past couple of years CPU chips
+        havent changed
+        much in the last ten years, There are currently only two major ompanies that manufacture CPUs; Intel and AMD.
+        They all
+        have their own versions that upgrade not necessariy over time but interms of efficiency. They do this inorder to
+        make
+        modern CPUs that are effciently and relatively affordable.CPUs are mostly sold depending on the task they
+        accomplish.
+        Cheaper ones are made for computers made for light use and the more expensive ones are uselful in heavy tasks
+        such as gamin, video editing
+        and handling other heavy software.These speeds are measured in clocks the slower ones being at 0.5GHz to the
+        faster ones at 5.0ghz, their efficiency is also measured in
+        cores which is basically one CPU divided into various portions from 1 known as dual core, 2 core CPUs are known
+        as dual cores, 4- quad core, 8 octacore and 10-decacore.
+        The increase in the number of cores also results in the increase of the maximum number of threads it can handle
+    </P>
+
+
+</main>
+
+<?php
+    include_once './layouts/main/footer.php';
